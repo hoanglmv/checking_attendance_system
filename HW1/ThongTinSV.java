29bbe51e94236dc4b23bc3bac610ff2d5763e73b
HMT <22022210@vnu.edu.vn>
2024-02-28 11:32:28 +0700

    Bài tập tuần 1

---
+++ HW1/ThongTinSV.java
@@ -0,0 +1,20 @@
+public class ThongTinSV {
+    public void bob(){
+        for(int i=9; i>=1; i--){
+            System.out.print(i + " bottles of beer on the wall, ");
+            System.out.println(i + " bottles of beer. ");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.\n");
+    }
+    public static void main(String[] args) {
+        ThongTinSV a = new ThongTinSV();
+        System.out.print("Ho va ten : HUNG MINH TUAN");
+        System.out.print("\tMSV : 22022210");
+        System.out.print("\tClass : K67 - K1");
+        System.out.print("\tUsername Github : 8MTi");
+        System.out.println("\tEmail : 22022210@vnu.edu.vn");
+        a.bob();
+
+    }
+}
